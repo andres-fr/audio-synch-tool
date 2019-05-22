45841acc61098fa9e8707dcc54c1fe5c915cfd88
andres-fr <fr_andres@msn.com>
2019-05-22 04:27:36 +0200

    Bump version: 0.1.0 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2019, Andres FR'
 author = 'Andres FR'
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup():
     setuptools.setup(
         name="audio_synch_tool",
         # the version is automatically handled by "bumpversion"
-        version="0.1.0",
+        version="0.2.0",
         author="Andres FR",
         description="A matplotlib interactive gui to align signals with high \
         precision",
