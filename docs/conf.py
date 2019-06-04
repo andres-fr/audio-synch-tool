beab403b92508bf993d04395a2b624b190465312
Andres Fernandez <fr_andres@msn.com>
2019-06-04 22:40:05 +0200

    Bump version: 0.6.0 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -32,10 +32,10 @@ copyright = '2019, Andres FR'
 author = 'Andres FR'
 
 # The short X.Y version
-version = '0.6.0'
+version = '1.0.0'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
