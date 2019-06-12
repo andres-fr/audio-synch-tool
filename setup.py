5ff3eef55bee456e643fa4e88f36ee78d8469930
andres-fr <fr_andres@msn.com>
2019-06-12 17:20:38 +0200

    Bump version: 1.3.2 → 1.3.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup():
     setuptools.setup(
         name="audio_synch_tool",
         # the version is automatically handled by "bumpversion"
-        version="1.3.2",
+        version="1.3.3",
         author="Andres FR",
         description="A matplotlib interactive gui to align signals with high \
         precision",
