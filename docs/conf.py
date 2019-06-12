7c2d3d092da2c4b6b638d37551a00370905bb1d5
andres-fr <fr_andres@msn.com>
2019-06-12 16:47:38 +0200

    Bump version: 1.3.0 → 1.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2019, Andres FR'
 author = 'Andres FR'
 
 # The short X.Y version
-version = '1.3.0'
+version = '1.3.1'
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.3.1'
 
 
 # -- General configuration ---------------------------------------------------
